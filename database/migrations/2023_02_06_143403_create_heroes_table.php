d2dbfd90c0764bcded7527f3826b7d105d2e18af
Emanuela91 <emanuela.marson91@gmail.com>
2023-02-06 16:14:16 +0100

    creato database

---
+++ database/migrations/2023_02_06_143403_create_heroes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('heroes', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name', 32)->nullable(false);
+            $table->string('last_name', 32)->nullable(false);
+            // must be in the past
+            $table->date('date_of_birth')->nullable(false);
+            // must be 0 or more
+            $table->integer('heigth')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('heroes');
+    }
+};
