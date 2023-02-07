6a0b32c6ab8574b87ac6fa5dc0c6bd54ef89827d
Emanuela91 <emanuela.marson91@gmail.com>
2023-02-07 11:20:23 +0100

    collegati tutti i dati della tabella al front end

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,13 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Hero;
 use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
     public function home()
     {
+        $heroes = Hero::all();
 
-        return view('pages.home');
+        return view('pages.home', compact('heroes'));
     }
 }
